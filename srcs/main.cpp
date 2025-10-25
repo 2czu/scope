1b9c40e75a63b49644186f0092e602ae57acd0ee
Paco Da Silva <pacda-si@f1br8s18.clusters.42paris.fr>
2025-10-25 14:13:30 +0200

    uniform positiong

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,14 +6,25 @@
 /*   By: pacda-si <pacda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 18:20:44 by pacda-si          #+#    #+#             */
-/*   Updated: 2025/10/11 11:55:42 by pacda-si         ###   ########.fr       */
+/*   Updated: 2025/10/25 14:09:13 by pacda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "../includes/scop.hpp"
 #include <SDL2/SDL.h>
+#include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
 GLuint VBO;
+GLint gScaleLocation = 0;
+
+const char *pVSFileName = "./shaders/shader.vs";
+const char *pFSFileName = "./shaders/shader.fs";
 
 static void createVertexBuffer()
 {
@@ -30,6 +41,117 @@ static void createVertexBuffer()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
+static bool readFile(const char* pFileName, std::string &outfile)
+{
+    std::ifstream f(pFileName);
+    bool ret = false;
+
+    if (f.is_open())
+    {
+        std::string line;
+        while(getline(f, line))
+        {
+            outfile.append(line);
+            outfile.append("\n");
+        }
+
+        f.close();
+        ret = true;
+    }
+    else
+    {
+        std::string fileName(pFileName);
+        std::cerr << "Couldn't open " << fileName << ".\n";
+    }
+    return (ret);
+    
+}
+
+static void addShader(GLuint shaderProgram, const char *shaderText, GLenum shaderType)
+{
+    GLuint shaderObject = glCreateShader(shaderType);
+    
+    if (!shaderObject)
+    {
+        std::cerr << "Error creating shader type : " << shaderType << "\n";
+        exit(1);
+    }
+
+    const GLchar *p[1];
+    p[0] = shaderText;
+    
+    GLint lengths[1];
+    lengths[0] = strlen(shaderText);
+
+    glShaderSource(shaderObject, 1, p, lengths);
+
+    glCompileShader(shaderObject);
+
+    GLint success;
+    glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        GLchar infoLog[1024];
+        glGetShaderInfoLog(shaderObject, 1024, NULL, infoLog);
+        std::cerr << "Error compiling shader type : " << shaderType << ", " << infoLog <<  "\n";
+        exit(1);
+    }
+
+    glAttachShader(shaderProgram, shaderObject);
+
+}
+
+static void compileShaders(void)
+{
+    GLuint shaderProgram = glCreateProgram();
+    
+    if (!shaderProgram)
+    {
+        std::cerr << "Error creating shader program\n";
+        exit(1);
+    }
+
+    std::string vs, fs; // vertex, fragment shader
+
+    if (!readFile(pVSFileName, vs) || !readFile(pFSFileName, fs))
+        exit(1);
+    
+    addShader(shaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+    addShader(shaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint success;
+    GLchar errLog[1204];
+
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderProgram, 1024, NULL, errLog);
+        std::cerr << "Error linking shader program : " << errLog <<  "\n";
+        exit(1);
+    }
+
+    gScaleLocation = glGetUniformLocation(shaderProgram, "gScale");
+    if (gScaleLocation == -1)
+    {
+        std::cerr << "Error getting uniform location of gScale : "<<  "\n";
+        exit(1);
+    }
+
+    glValidateProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_VALIDATE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderProgram, 1024, NULL, errLog);
+        std::cerr << "Error linking shader program : " << errLog <<  "\n";
+        exit(1);
+    }
+
+    glUseProgram(shaderProgram);
+
+}
+
 static void render()
 {
     // static GLclampf red = 0.0f;
@@ -40,6 +162,10 @@ static void render()
 
     // glClearColor(red, 0.0f, 0.0f, 1.0f);
 
+    static float Scale = 0.0f;
+    Scale += 0.05f;
+    glUniform1f(gScaleLocation, sinf(Scale));
+    
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -50,6 +176,8 @@ static void render()
     
     glDisableVertexAttribArray(0);
 }
+
+
 
 int main(int argc, char** argv)
 {
@@ -100,7 +228,10 @@ int main(int argc, char** argv)
     // Vector3f vertices[1];
     // vertices[0] = Vector3f();
     // vertices[0].print();
+
     createVertexBuffer();
+
+    compileShaders();
 
     bool running = true;
     SDL_Event event;
